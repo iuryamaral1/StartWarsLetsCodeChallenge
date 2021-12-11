61a47b7747fb09df17d60cbd974f4a84cef7b2bb
Iury Amaral <inv.iamaral@ifood3rd.com>
2021-12-10 22:33:22 -0300

    Initial commit

---
+++ src/main/java/com/letscode/starwarsresistence/StarWarsResistenceLetsCodeApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.starwarsresistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsResistenceLetsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsResistenceLetsCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letscode/starwarsresistence/StarWarsResistenceLetsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.starwarsresistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsResistenceLetsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
